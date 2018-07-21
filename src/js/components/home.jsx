ab74acc480b947a3aea194d1e3629a7f231e5198
pilotocheg <slavutich.org@gmail.com>
2018-07-21 16:41:00 +0300

    home.jsx changed

---
+++ src/js/components/home.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import pic from '../../img/pic.jpg';
+import pic1 from '../../img/pic-1.jpg';
+import React from 'react';
+
+export default function Home() {
+  return(
+    <div className="container">
+      <div className="s_width">
+        <header>
+          <h1>
+            ВЕСЕ<span className="stroke">Л</span><br/>
+            <span id="big-letter">A</span>
+            <span id="second-word">
+              <span className="stroke">Б</span>ЕТКА
+            </span>
+          </h1>
+          <p className="title_primary">Інтерактивна абетка для дітей</p>
+        </header>
+        <div className="navigation">
+          <a className="nav_link" id="link-1" href=""><span>В</span>ЧИТИСЯ</a>
+          <a className="nav_link" id="link-2" href=""><span>Г</span>РАТИ</a>
+          <a className="nav_link" id="link-3" href=""><span>П</span>РО <span>Н</span>АС</a>
+        </div>
+        <div className="content">
+          <div className="description">
+            <div id="learn" className="learn">
+              <h2><span>В</span>ЧИТИСЯ</h2>
+              <p>Ця програма допоможе дітям вивчати абетку, завдяки яскравим картинкам та звукам. Також має цікавий <span>ігровий</span> режим</p>
+              <Link to="/learn">
+                <button>ВЧИТИСЯ</button>
+              </Link>
+            </div>
+            <div id="play" className="play">
+              <h2><span>Г</span>РАТИ</h2>
+              <p>Цей режим поєднує в собі навчання та гру. Він допоможе дітям краще засвоїти літери,та зрозуміти їх зв'язок із словами</p>
+              <Link to="/game">
+                <button>ВЧИТИСЯ</button>
+              </Link>
+            </div>
+          </div>
+          <div className="images">
+            <img id="slider" src={pic} />
+            <img src={pic1}/>
+          </div>
+          <button className="nav" id="nav_left">&lsaquo;</button>
+          <button className="nav" id="nav_right">&rsaquo;</button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GameApp from './components/game-app';
 import LearnApp from './components/learn-app';
+import Home from './components/home';
 import ControlBtns from './components/control-btns';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
-  <div id="main-div">
-    <ControlBtns />
-    <Switch>
-      <Route exact path="/learn" component={LearnApp}/>
-      <Route exact path="/game" component={GameApp}/>
-      {/* <Route path="*" component={<h1>PAGE NOT FOUND</h1>}/> */}
-    </Switch>
-  </div>
+    <div>
+      <div id="main-div">
+        <ControlBtns />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/learn" component={LearnApp}/>
+          <Route exact path="/game" component={GameApp}/>
+          <Route path="*" component={<h1>PAGE NOT FOUND</h1>}/> 
+        </Switch>
+      </div>
+      {/* <Home /> */}
+    </div>
   </Router>,
   document.getElementById('root')
 )
